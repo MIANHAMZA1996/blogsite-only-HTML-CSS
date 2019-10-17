0f316901965713008b91e5e0bea585f70be59a52
Muhammad Hamza <56311699+MIANHAMZA1996@users.noreply.github.com>
2019-10-17 18:25:59 +0500

    Initial commit

---
+++ copy CSS file.php
@@ -0,0 +1,413 @@
+)
+/*whole body */
+.body
+{
+	height: all;
+	float: left;
+	width: 100%;
+}
+.mainbody
+{
+	height: all;
+	float: left;s
+	width: 1333px;
+	background-color: #2D3136;
+}
+	/*HEADER STARTS*/
+		.header
+		{
+			height: 45px;
+			width: 1333px;
+			background-color: #272727;
+
+		}
+		/* logo div*/
+		.logo
+		{
+			height: 40px;
+			width: 150px;
+			float: left;
+
+		}
+		/*logo img details*/
+		.logoimg
+		{
+			height: 40px;
+			width: 40px;
+			margin-top: 3px;
+			margin-left: 25px;
+
+		}
+		/*menu bar*/
+		.menubar
+		{
+			height: 120;
+			width: 930px;
+			font-size:18px; 
+			font-family: sans-serif; 
+			color: #FFFFFF;
+			text-align: center; 
+			padding-top: 12px;
+			float: left;
+			float: left;
+			margin-left: -150px;
+
+		}
+		/*menu bar css for all text*/
+		.text1
+		{
+				color: #FFFFFF;
+				padding: 10px;
+		}
+		/*login and sign up button*/
+		.option
+		{
+			font-size:18px; 
+			font-family: sans-serif;					
+			padding-top: 10px;
+			float: right;
+		}
+	/*HEADER ENDS*/
+
+/*HOME PAGE STARTS*/
+
+	/* CONTAINER STARTS*/
+		.container
+		{
+			height: all;
+			width: 1333px;
+
+		}
+			/*posts*/
+			.text2
+			{
+				font-size:20px; 
+				font-family: sans-serif;					
+				font-style: oblique;
+				color: white;
+				/*margin-top: 15px;*/	
+			}
+			.post
+			{
+				height: 190px;
+				margin-left: 25px;
+				margin-top: 20PX
+			}
+			/*post image div*/
+			.postpic
+			{
+				height: 200px;
+				width: 275px;
+			}
+			/*image details*/
+			.postimg
+			{
+				height: 150px;
+				width: 300px;
+				margin-top: 8px;
+				float: left;
+			}
+			.paragraph
+			{
+				height: 150px;
+				width: 602px;
+				padding-top: 6px;
+				font-size: 16px;
+				color: white;
+				padding-left: 380px;
+				text-align: justify;
+			}
+
+	/* CONTAINER ENDS*/
+
+/*HOME PAGE ENDS*/
+
+/* FOOTER*/
+	.footer
+	{
+		height: 50px;
+		width: 1333px;
+
+		
+	}
+	.footer1
+	{
+		color: white;
+		margin-top: 10px;
+		padding: 18px;
+		background-color: #272727;
+		text-align: center;
+	}
+/*FOOTER*/
+
+
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ABOUT US/////////////////////////////////*/
+
+/*paragraph*/
+.paragraph1
+{
+	font-size: 23px; 
+	font-family: sans-serif;
+	margin-left: 55px; 
+	margin-top: 50px; 
+	color: #FFFFFF;
+	width: 1226px;
+	text-align: justify;
+
+}
+
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\CONTACT US/////////////////////////////////*/
+
+
+.contactimg
+{
+	height: 150px;
+	width: 465px;
+	margin-left: 212px;
+	border-radius: 5px;
+}
+.text3
+{
+	text-align: center;
+	color: #FFFFFF;
+	font-family: sans-serif;
+
+}
+.contactform
+{
+	height: auto;
+	width: 1333px;
+	color: white;
+	font-family: sans-serif;
+ 	border-radius: 5px;
+    background-color: #2D3136;
+    padding: 10px;
+    float: left;
+    padding-left: 500px;
+
+}
+.text4
+{
+
+}
+/* CONTACT*/
+
+
+/* Style inputs */
+input[type=text], select, textarea {
+    width: 237px;
+    padding: 9px;
+    color: #FFFFFF;
+    font-family:sans-serif;
+    border-bottom: 1px solid #ccc;
+    margin-top: 6px;
+    margin-bottom: 15px;
+    background: transparent;
+    border-radius: 5px;
+}
+.newpostform{
+width:500px; margin-left: 495px; text-align: justify;	
+}
+input[type=submit] {
+    background-color: #1C8ADB;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+input[type=submit]:hover {
+    background-color: #272727;
+}
+/* Style inputs end */
+
+
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Posts/////////////////////////////////*/
+
+
+.newpost
+{
+	width: 150px;
+    text-align: center;
+    font-family: sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    height: 30px;    
+    margin-top: 5px;
+    border-radius: 5px;
+    float: right;
+    margin-top: 20px;
+    margin-right: 20px;
+	margin-bottom: 10px;
+}
+.posttable
+{
+
+	width: 1300px; 
+	height: 300px; 
+	margin-left: 18px;
+	font-family: sans-serif; 
+	font-size: 20px;
+	box-shadow:   1px 1px rgba(0, 0, 0, 0.8); 
+	margin-top: 30px; 
+	border-radius: 10px; 
+	border:1px solid #9b9393;
+	text-align: justify;
+	background-color: #272727;
+	color: white;
+}
+/**/
+}
+
+/*.rowpost
+{
+	text-align: center;
+}*/
+.buttonpost
+{
+	background-color:#272727; 
+	color: white; 
+	border-radius: 5px;
+}
+
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\NEW POST/////////////////////////////////*/
+.text5
+{
+	font-family: sans-serif;
+	color: white;
+	margin-top:  20px;
+}
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\USERS/////////////////////////////////*/
+
+
+.usertable
+{
+	width: 1300px; 
+	height: 200px; 
+	margin-left: 18px;
+	font-family: sans-serif; 
+	font-size: 20px;
+	box-shadow:   1px 1px rgba(0, 0, 0, 1); 
+	margin-top: 30px; 
+	border-radius: 10px; 
+	border:1px solid #9b9393;
+	text-align: center;
+	background-color: #272727;
+	color: white;
+}
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\login/////////////////////////////////*/
+
+.logincontainer
+{
+	height: 465px;
+	width: 290px;
+	margin-left: 500px;
+	background-color: #272727;
+	margin-top: 110px;
+	border-radius: 10px;
+	padding:20px;
+
+}
+
+.loginlogo
+{
+	height: 110px;
+	width: 110px;
+	float: left;
+	margin-left: 607px;
+	margin-top: 33px;
+}
+.text6
+{
+	 font-family: sans-serif;
+	 font-size: 39px; 
+	 color: white;
+	 margin-left: 55px;
+
+}
+.text7
+{
+	font-family: sans-serif;
+	font-size: 25px;
+	margin-left: 20px;
+	margin-bottom: 30px;
+	margin-top: 10px;
+	color: white;
+
+}
+.password
+{
+	width: 237px;
+	padding: 12px;
+	color: #FFFFFF;
+	font-family: sans-serif;
+	border-bottom: 1px solid #ccc;
+	margin-top: 6px;
+	margin-bottom: 15px;
+	margin-left: 18px;
+	background: transparent;
+	border-radius: 5px;
+}
+.text8
+{
+	font-family: sans-serif;
+	font-size: 15px;
+	margin-left: 20px;
+	color: white;
+
+}
+
+/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\login/////////////////////////////////*/
+
+.signupform
+{
+	height: 560px;
+	width: 660px;
+	background-color: #272727;
+	margin left: 35px;
+	border-radius: 20px;
+	box-shadow: 0 0 0 .5px #000000 ;
+}
+.dataentry
+{
+	height: 560
+	width:320;
+	/*background-color: blue;	*/
+	float: left;			
+	border-left: 1px white;
+}
+.text9
+{
+	margin-left: 100px;
+	margin-top: 65px;
+	font-family: sans-serif;
+	color: white;
+
+}
+
+.sociallink
+{
+	height: 560
+	width:330;
+	/*background-color: yellow;*/
+	float: right;
+}
+.text10
+{
+	margin-right: 80px;
+	margin-top: 65px;
+	font-family: sans-serif;
+	color: white;
+}
+.text11
+{
+	margin-left: 50px;
+	font-family: sans-serif;
+	color: white;
+
+}
+.uname
+{
+
+}
